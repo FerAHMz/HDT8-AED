7238569bb86913b51f47da6a3378b9e62fd74d6f
Fernando Hernandez <ferandree270904@gmail.com>
2024-04-07 12:45:26 -0600

    Cambios Main Class, Archivo TXT
    Se creo el archivo txt y se realizaron cambios para que el archivo Main pueda leer correctamente el archivo por Maven

--- hdt8/src/main/java/uvg/edu/gt/Main.java
+++ hdt8/src/main/java/uvg/edu/gt/Main.java
@@ -2,6 +2,7 @@ package uvg.edu.gt;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -9,18 +10,19 @@ public class Main {
         VectorHeap<Paciente> colaEmergencias = new VectorHeap<>();
 
         try {
-            File archivoPacientes = new File("pacientes.txt");
+            File archivoPacientes = new File("hdt8/pacientes.txt");
             Scanner lectorArchivo = new Scanner(archivoPacientes);
             while (lectorArchivo.hasNextLine()) {
                 String linea = lectorArchivo.nextLine();
-                String[] datosPaciente = linea.split(",");
+                // Asegúrate de que el split se hace correctamente
+                String[] datosPaciente = linea.split(", ");
                 if (datosPaciente.length == 3) {
                     Paciente paciente = new Paciente(datosPaciente[0], datosPaciente[1], datosPaciente[2].charAt(0));
                     colaEmergencias.insert(paciente);
                 }
             }
             lectorArchivo.close();
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             System.out.println("No se encontró el archivo pacientes.txt");
             e.printStackTrace();
             return;
@@ -28,7 +30,7 @@ public class Main {
         System.out.println("Atendiendo a los pacientes en orden de prioridad:");
         while (!colaEmergencias.isEmpty()) {
             Paciente pacienteAtendido = colaEmergencias.remove();
-            System.out.println("Atendido a: " + pacienteAtendido.getNombre() + " _ " + pacienteAtendido.getSintoma() + " (Prioridad: " + pacienteAtendido.getCodigoEmergencia() + ")");
+            System.out.println("Atendido a: " + pacienteAtendido.getNombre() + " - " + pacienteAtendido.getSintoma() + " - Prioridad: " + pacienteAtendido.getCodigoEmergencia());
         }
     }
 }
